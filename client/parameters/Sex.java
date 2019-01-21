eb170b9274312d2cdc941e520cac30bde4473714
Equento <axpehuct95@gmail.com>
2019-01-22 00:14:28 +0300

    changes for filter

--- client/parameters/Sex.java
+++ client/parameters/Sex.java
@@ -1,9 +1,7 @@
 package client.parameters;
 
 public class Sex extends Filter {
-    /*Может лучше заменить на String? Так проще в будущем будет разобраться где Male, а где Female
-    и не будет непонятных true и false.
-     */
+
     private String sex;
 
     public Sex(String filterId, String sex) {
